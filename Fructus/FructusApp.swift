d2e9d124f33ceefebba9e1cf9d497719cb4de8c3
hustin-aquino <justinmosesbitancor@gmail.com>
2024-10-07 11:15:31 +0800

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by justin.bitancor on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
